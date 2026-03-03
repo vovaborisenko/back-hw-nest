3e55b15169a79d87efd0b3b9ea76d7cea641e35e
Uladzimir Barysenka <vovaborisenko@live.com>
2026-03-03 20:58:10 +0300

    add vercel config

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,4 +1,5 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
@@ -7,7 +8,11 @@ import { BloggersPlatformModule } from './modules/bloggers-platform/bloggers-pla
 
 @Module({
   imports: [
-    MongooseModule.forRoot('mongodb://localhost:27017/incubator-hw'),
+    ConfigModule.forRoot(),
+    MongooseModule.forRoot(
+      process.env.MONGO_URL || 'mongodb://localhost:27017/incubator-hw',
+      { dbName: process.env.DB_NAME },
+    ),
     UserAccountsModule,
     BloggersPlatformModule,
   ],
--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,7 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  app.enableCors();
   app.useGlobalPipes(
     new ValidationPipe({
       transform: true,
@@ -14,4 +15,5 @@ async function bootstrap() {
 
   await app.listen(process.env.PORT ?? 3000);
 }
+
 bootstrap();
