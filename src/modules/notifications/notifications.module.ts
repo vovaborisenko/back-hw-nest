41eec5cf289483129992f963ee3a706041950990
Uladzimir Barysenka <vovaborisenko@live.com>
2026-03-27 22:36:30 +0300

    add notifications config

--- src/modules/notifications/notifications.module.ts
+++ src/modules/notifications/notifications.module.ts
@@ -2,23 +2,32 @@ import { Module } from '@nestjs/common';
 import { MailerModule } from '@nestjs-modules/mailer';
 import { EmailService } from './email.service';
 import { EmailManager } from './email.manager';
+import { NotificationsConfig } from './notifications.config';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [
-    MailerModule.forRoot({
-      transport: {
-        service: 'gmail',
-        auth: {
-          user: 'v.borisenko.maxbit@gmail.com',
-          pass: 'vmxipjzxklwagyya',
-        },
-      },
-      defaults: {
-        from: '"no-reply" <v.borisenko.maxbit@gmail.com>',
+    MailerModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [NotificationsConfig],
+      extraProviders: [NotificationsConfig],
+      useFactory(config: NotificationsConfig) {
+        return {
+          transport: {
+            service: config.emailTransportService,
+            auth: {
+              user: config.email,
+              pass: config.emailPassword,
+            },
+          },
+          defaults: {
+            from: `"${config.emailName}" <${config.email}>`,
+          },
+        };
       },
     }),
   ],
-  providers: [EmailService, EmailManager],
+  providers: [EmailService, EmailManager, NotificationsConfig],
   exports: [EmailService],
 })
 export class NotificationsModule {}
